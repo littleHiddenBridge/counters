05a3569b7c116d271bf3f23a1af6ae12bdf50f53
Tito Lopez <cesar.augusto.lopez2001@gmail.com>
2022-07-25 16:03:49 -0400

    diseable dicrement button

--- appshopcart/src/components/counter.jsx
+++ appshopcart/src/components/counter.jsx
@@ -6,19 +6,25 @@ class Counter extends Component {
   render() {
 
     return (
-      <div>
+      <div className="row">
+        <div className="col-1">
         <h4>{this.props.id}</h4>
         <span className={this.getBadgesClasses()}>{this.formatCount()}</span>
+        </div>
+        <div className="col">
         <button onClick={() => this.props.onIncrement(this.props.counter)} className="btn btn-secondary m-3">
-          Increment
+           +
         </button>
-        <button onClick={() => this.props.onDecrement(this.props.counter)} className="btn btn-secondary">
-          Decrement
+        <button onClick={() => this.props.onDecrement(this.props.counter)} className="btn btn-secondary m-2" disabled={this.props.counter.value === 0 ? 'disabled': ''}>
+           -
         </button>
-        <button onClick={() => this.props.onDelete(this.props.counter.id)} className="btn btn-danger btn-sm m-2"> Delete</button>
+        <button onClick={() => this.props.onDelete(this.props.counter.id)} className="btn btn-danger btn-sm"> Delete</button>
+        </div>
       </div>
+               
     );
   }
+ 
 
   formatCount() {
     const { value } = this.props.counter;
